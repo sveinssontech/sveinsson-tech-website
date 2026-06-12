a6a9f1a6016a1b4f1e74e9c47a46e33670a0d133
sveinssontech <sveinssontech@pm.me>
2026-06-12 16:03:12 +0200

    Some home page work and text.

--- src/components/pages/HomeBody.jsx
+++ src/components/pages/HomeBody.jsx
@@ -1,10 +1,25 @@
-import PageBody from '../PageBody/PageBody'
-import LazyScene3D from '../Scene3D/LazyScene3D'
+import PageBody from "../PageBody/PageBody";
+import LazyScene3D from "../Scene3D/LazyScene3D";
+import FacePicture from "../../images/Face.jpg";
+import * as THREE from "three";
 
 export default function HomeBody() {
+  const texture = new THREE.TextureLoader().load(FacePicture);
+
   return (
     <PageBody>
-      <LazyScene3D />
+      <LazyScene3D>
+        <mesh>
+          <boxGeometry args={[6, 6, 6]} />
+          <meshBasicMaterial map={texture} toneMapped={false} />
+        </mesh>
+      </LazyScene3D>
+      <h1>Hello! My name is Magni</h1>
+      <p>
+        I run the tech studio Sveinsson Tech. I like experimenting with new and
+        exiting tech, making games and playing games as well. Hope those that
+        find their way here will enjoy whatever I have been working on!
+      </p>
     </PageBody>
-  )
+  );
 }
